20d30106491ca08174b4372a8103da4391996861
talestk <tales.takemiya@gmail.com>
2024-09-27 10:41:38 -0300

    StripComments tough problem for edge cases

--- src/main/java/org/example/StripComments.java
+++ src/main/java/org/example/StripComments.java
@@ -1,11 +1,52 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class StripComments {
 
-    public static String stripComments(String text, String[] commentSymbols) {
-
-        return "";
+    // felt bad looking at this solution after 1h trying to get all the tests to pass
+    public static String stripCommentsBestSolution(String text, String[] commentSymbols) {
+        String pattern = String.format(
+                "[ ]*([%s].*)?$",
+                Arrays.stream( commentSymbols ).collect( Collectors.joining() )
+        );
+        return Arrays.stream( text.split( "\n" ) )
+                .map( x -> x.replaceAll( pattern, "" ) )
+                .collect( Collectors.joining( "\n" ) );
     }
 
+
+    public static String stripComments(String text, String[] commentSymbols) {
+        String res = Arrays.stream(text.split("\n"))
+                .map(line -> getStrippedLine(line, commentSymbols))
+                .collect(Collectors.joining());
+
+        return res.substring(0, res.lastIndexOf("\n"));
+    }
+
+    private static String getStrippedLine(String line, String[] commentSymbols) {
+        int firstSymbolIndex = Integer.MAX_VALUE;
+
+        for (String symbol : commentSymbols) {
+            int currentSymbolIndex = line.indexOf(symbol);
+            // if found symbol then set the fsi to its index
+            if (currentSymbolIndex >= 0)
+                firstSymbolIndex = Math.min(firstSymbolIndex, currentSymbolIndex);
+        }
+
+        // if there were no matched symbols just trim the line
+        if (firstSymbolIndex == Integer.MAX_VALUE)
+            return rightTrim(line) + "\n";
+        return rightTrim(line.substring(0, firstSymbolIndex)) + "\n";
+    }
+
+    private static String rightTrim(String s) {
+        int i = s.length() - 1;
+        while (i >= 0 && Character.isWhitespace(s.charAt(i)))
+            i--;
+
+        return s.substring(0, i + 1);
+    }
 }
 
